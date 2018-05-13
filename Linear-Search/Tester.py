b753df1aaf77d333926f4357b8a15edfa3ff4197
Medi Assumani <mediassumani49@gmail.com>
2018-05-12 20:57:33 -0400

    adding

---
+++ Linear-Search/Tester.py
@@ -0,0 +1,9 @@
+# This is a tester file to benchmark the performance of the linear search
+from linear_search import linear_search
+
+def main():
+    list_one = [10,20,40,50,56,2,4,5,7,8,10,120,67,45]
+    print linear_search(list_one,120)
+
+if __name__ == "__main__":
+    main()

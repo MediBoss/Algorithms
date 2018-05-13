5bb39a83b5eed27d9b09e968f478fc3650e90e32
Medi Assumani <mediassumani49@gmail.com>
2018-05-13 10:15:20 -0400

    adding

--- Linear-Search/linear_search.py
+++ Linear-Search/linear_search.py
@@ -2,7 +2,7 @@
 # Linear sort is commonly used for small range of data in a list since it parses the data one by one.
 
 
-# Function to lineary look for item in list and returns its location
+# Function to lineary look for item in list and returns its location or -1 if it's not found
 def linear_search(list, item):
 
     for index in range(len(list)):

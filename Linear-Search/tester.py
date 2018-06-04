b993c2bdfb60e8f59b34356c021463a6d0162309
MediBoss <mediassumani49@gmail.com>
2018-06-03 22:02:01 -0400

    wrapping up

--- Linear-Search/tester.py
+++ Linear-Search/tester.py
@@ -4,6 +4,8 @@ from algorithm_timer import timing_function
 
 @timing_function
 def tester():
+
+    item = 20
     list_one = [10,20,40,50,56,2,4,5,7,8,10,120,67,45,
     24,27,62,65,78,28,77,69,65,58,66,88,100,73,88,16,
     39,58,68,49,59,72,50,76,6782,36,100,22,17,81,70,42,
@@ -11,7 +13,7 @@ def tester():
     17,42,56,89,50,94,72,68,43,82,53,82,11,94,63,74,67,
     65,77,96,95,61,85,9,26,91,91,44,83,7,90,71,37,73,85,14]
 
-    print "Number 14 is found at position : {}".format(linear_search(list_one,14))
+    print "Number {} is found at position : {}".format(item,linear_search(list_one,item))
 print(tester())
 
 def main():

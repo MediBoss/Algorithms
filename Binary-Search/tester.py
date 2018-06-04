2f7a9b060cf04f1efdeda94bf9fe345cd1f7972c
MediBoss <mediassumani49@gmail.com>
2018-06-03 21:48:02 -0400

    test ran

--- Binary-Search/tester.py
+++ Binary-Search/tester.py
@@ -6,14 +6,16 @@ from algorithm_timer import timing_function
 
 @timing_function
 def tester():
-    item = 10
+    item = 64
     list_one = [10,20,40,50,56,60,64,75,87,88,90,92,97,105]
 
-    print "Number {} is found at position : {}".format(item,linear_search(list_one,item))
+    print "Number {} is found at position : {}".format(item,binary_search(list_one,item))
 print(tester())
 
 def main():
-    tester() # Output :
+    tester()
+    # Output : when item is 10 : it took 5 seconds
+    # Ouput : when item is 64 : 3 seconds
 
 if __name__ == "__main__":
     main()

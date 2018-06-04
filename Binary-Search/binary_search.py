3c2d55fca03cee657c369b21d288b5238582c19f
MediBoss <mediassumani49@gmail.com>
2018-06-03 21:10:47 -0400

    initial work

--- Binary-Search/binary_search.py
+++ Binary-Search/binary_search.py
@@ -1,1 +1,14 @@
+# Binary Search : Search a sorted array by repeatedly dividing the search interval in half
 
+def binary_search(list,item):
+    """
+
+    @param list: The list of elements
+    @param item: The element to be searched in the list
+    Time complexity :
+    Best Case Scenario : The list is sorted
+    Worst Case Scenario:
+
+    """
+
+    

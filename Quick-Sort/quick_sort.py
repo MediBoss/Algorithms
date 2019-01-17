03f854e1c727de1012012d5a4a20eef8b7a4119e
Medi Assumani <mediassumani49@gmail.com>
2019-01-17 07:36:22 -0800

    [Add] complete partion function

--- Quick-Sort/quick_sort.py
+++ Quick-Sort/quick_sort.py
@@ -1,8 +1,8 @@
 
-def partition(unsoted_arr, first_index, last_index):
-    ''' Helper function to sort the chopped array with a pivot'''
+def partition(unsorted_arr, first_index, last_index):
+    ''' Helper function to sort a chunk of unsorted array with a pivot'''
 
-    pivot_element = unsoted_arr[first_index]
+    pivot_element = unsorted_arr[first_index]
     pivot_index = first_index
     index_of_last_element = last_index
     greater_than_pivot_index = first_index + 1
@@ -10,12 +10,22 @@ def partition(unsoted_arr, first_index, last_index):
 
     while True:
 
-        while unsoted_arr[greater_than_pivot_index] < pivot and greater_than_pivot_index < last_index:
+        while unsorted_arr[greater_than_pivot_index] < pivot and greater_than_pivot_index < last_index:
             greater_than_pivot_index += 1
 
-        while unsoted_arr[less_than_pivot_index] > pivot and less_than_pivot_index >= first_index:
+        while unsorted_arr[less_than_pivot_index] > pivot and less_than_pivot_index >= first_index:
             less_than_pivot_index -= 1
-        
+
+        if greater_than_pivot_index < less_than_pivot_index:
+            unsorted_arr[greater_than_pivot_index], unsorted_arr[less_than_pivot_index] = unsorted_arr[less_than_pivot_index], unsorted_arr[greater_than_pivot_index]
+        else:
+            break
+
+    unsorted_arr[pivot] = unsorted_arr[less_than_pivot_index]
+    unsorted_arr[less_than_pivot_index] = pivot
+
+    return less_than_pivot_index
+
 
 
 

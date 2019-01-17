24bd9b2edeb47b439a2d055e8286eccd676c4b82
Medi Assumani <mediassumani49@gmail.com>
2019-01-17 06:45:41 -0800

    [Add] BigO notation and algorithm pseudocode

--- Selection-Sort/selection_sort.py
+++ Selection-Sort/selection_sort.py
@@ -1,25 +1,23 @@
-
-# O(n)
-def find_smallest(arr):
-    ''' Helper function to find the smallest element in a sequence '''
-    min = arr[0]
-    for elem in arr:
-        if elem < min:
-            min = elem
-
-    return min
-
-
+# Best Case : O(n) ^2 even if list is sorted, we must still check each element with 2 inner loops
+# Worst Case : O(n) ^ 2 since we must traverse the entire list with 2 inner loops
 def selection_sort(arr):
-
-    smallest = find_smallest(arr)
+''' Sort the elements in arr from left to right'''
     for i in range(len(arr)):
         for j in range(i+1, len(arr)):
             if arr[j] < arr[i]:
                 arr[i], arr[j] = arr[j], arr[i]
 
 
+'''
+        ALGORITHM:
+        *********
 
+1. Start outter Loop from i=0 to length of list
+2. Start inner Loop from i=i+1 to length of list
+3. Check of current element in inner loop is less than current element in outter loop
+4 If yes, swap both
+
+'''
 
 
 
--- Selection-Sort/tester.py
+++ Selection-Sort/tester.py
@@ -8,8 +8,7 @@ def main():
     list_one = [5,4,8,1,9,34,12]
     selection_sort(list_one)
     for i in range(len(list_one)):
-        print ("%d" %list[i])
-
+        print ("%d" %list_one[i])
 
 
 if __name__ == '__main__':

fd2e61e847685e2fbf269687ebb1f901874e5690
Medi Assumani <mediassumani49@gmail.com>
2019-01-22 11:12:58 -0800

    [Add] factorial problem using recursion

---
+++ Recursion/factorial.py
@@ -0,0 +1,21 @@
+
+'''
+    Factorial is the product of an integer and all the integers below it; e.g.
+    factorial four (4!) is equal to 24.
+
+'''
+def factorial(number):
+
+    # Base case - tells the recursion when to terminate
+    if number == 0:
+        return 1
+    else:
+    # Recursive case - calls the function within
+        return number * factorial(number - 1)
+
+def main():
+
+    print(factorial(5))
+
+if __name__ == "__main__":
+    main()

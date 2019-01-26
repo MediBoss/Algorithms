301334d7e1393170b65f45e0d6ee0957752104ae
Medi Assumani <mediassumani49@gmail.com>
2019-01-26 09:42:23 -0800

    [Add] solution to array permuatation

--- Recursion/array_permutation.py
+++ Recursion/array_permutation.py
@@ -5,8 +5,31 @@ Given an array of integers, return the set of all permutations.
 
 def permutate(arr):
 
-    # Identify base case
-    if len(arr) == 0 or len(arr) == 1:
-        return arr
+    # Base Case
+    if len(arr) <= 1:
+        return [arr]
 
-    # Identify recursive case
+    # Recursive Case
+    else:
+
+        last_element = arr[-1]
+        result = permutate(arr[:-1])
+        permutations = []
+        for p in result:
+            for i in range(len(arr)):
+                left = p[:i]
+                right = p[i:]
+                permutations.append(left + [last_element] + right)
+
+    return permutations
+
+def main():
+
+    list_one = [1,2]
+    list_two = [1,2,3]
+
+    permutate(list_one)
+
+
+if __name__ == "__main__":
+    main()
